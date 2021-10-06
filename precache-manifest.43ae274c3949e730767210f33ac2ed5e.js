0fc0df30a19659658fa0924a5679987c20d0126e
João Pedro Reis De Mello <joaoreisjpk@gmail.com>
2021-10-05 23:24:23 -0300

    Add files via upload

---
+++ precache-manifest.43ae274c3949e730767210f33ac2ed5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36506d50ed895b5093a6c711bb18948c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e22e4263c7b95d135f8",
+    "url": "./static/css/main.1c7d4db0.chunk.css"
+  },
+  {
+    "revision": "a9019b321bf55af9433a",
+    "url": "./static/js/2.ed4662c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.ed4662c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e22e4263c7b95d135f8",
+    "url": "./static/js/main.3ace6688.chunk.js"
+  },
+  {
+    "revision": "3bad4da90a1d62b2ed0b",
+    "url": "./static/js/runtime-main.6cc6b37c.js"
+  },
+  {
+    "revision": "23059254869e1fa78abc861f0eb32f16",
+    "url": "./static/media/logo.23059254.jpeg"
+  }
+]);
